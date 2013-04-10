f2bb99c5fccdb698edc2298d0b95bf535d4faa81
mathieucarbou <mathieu.carbou@gmail.com>
2013-04-10 12:42:45 -0400

    added bango cancellation

--- src/main/groovy/com/ovea/billing/support/BangoConnector.groovy
+++ src/main/groovy/com/ovea/billing/support/BangoConnector.groovy
@@ -16,6 +16,8 @@
 package com.ovea.billing.support
 
 import com.ovea.billing.*
+import com.ovea.tadjin.util.Resource
+import groovy.text.SimpleTemplateEngine
 
 import java.util.logging.Logger
 
@@ -29,12 +31,22 @@ class BangoConnector implements BillingCallback {
 
     private final BillingConfig config
     private final def bango = [
-        callback: '/callback/' + BillingPlatform.bango
+        callback: '/callback/' + BillingPlatform.bango,
+        url: 'https://webservices.bango.com/subscriptions/service.asmx',
+        username: '',
+        password: '',
+        tmpl: [
+            status: new SimpleTemplateEngine().createTemplate(Resource.from('classpath:com/ovea/billing/support/bango-status.xml').readAsString()),
+            cancel: new SimpleTemplateEngine().createTemplate(Resource.from('classpath:com/ovea/billing/support/bango-cancel.xml').readAsString())
+        ]
     ]
 
     BangoConnector(BillingConfig config) {
         this.config = config
         bango.callback = config.url + bango.callback
+        def pcfg = config.getPlatformConfig(BillingPlatform.bango)
+        bango.username = pcfg.username
+        bango.password = pcfg.password
     }
 
     @Override
@@ -61,15 +73,26 @@ class BangoConnector implements BillingCallback {
                     break
 
                 case BillingEventType.CALLBACK_REQUEST:
-                    if (!e.request.getParameter('u') || !e.request.getParameter('t')) {
-                        throw new IllegalArgumentException('Missing subscription id and user info')
-                    }
                     e.data << [
                         reference: e.request.getParameter('p'),
-                        id: "${e.request.getParameter('u')}|${e.request.getParameter('t')}", // BangoUserId|BangoSubscriptionToken
+                        id: e.request.getParameter('subid'),
                         period: config.getProductConfig(e.product).period
                     ]
-                    e.type = BillingEventType.CALLBACK_BUY_ACCEPTED
+                    if (!e.data.id) {
+                        throw new IllegalArgumentException('Missing subscription id')
+                    }
+                    e.data << status(e)
+                    switch (e.data.status) {
+                        case 'OK':
+                            e.type = BillingEventType.CALLBACK_BUY_ACCEPTED
+                            break
+                        case 'WAITING':
+                            e.type = BillingEventType.CALLBACK_BUY_PENDING
+                            break
+                        default:
+                            e.type = BillingEventType.CALLBACK_BUY_REJECTED
+                            break
+                    }
                     break
 
                 case BillingEventType.CANCEL_REQUEST_ACCEPTED:
@@ -77,20 +100,64 @@ class BangoConnector implements BillingCallback {
                         throw new IllegalArgumentException('Missing subscription id')
                     }
                     e.type = BillingEventType.CANCEL_COMPLETED
-                    //TODO - call bango
-                    /*e.data << status(e)
-                    if (e.data.status == 'ACTIVE') {
+                    e.data << status(e)
+                    if (e.data.status == 'OK' || e.data.status == 'WAITING') {
                         e.data << cancel(e)
-                        if (e.data.redirect) {
-                            e.type = BillingEventType.CANCEL_PENDING
-                        }
-                    }*/
-                    e.answer([
-                        redirect: e.data.redirect
-                    ])
+                    }
                     break
             }
         }
 
     }
+
+    def status(BillingEvent e) {
+        def req = bango.tmpl.status.make([
+            event: e,
+            username: bango.username,
+            password: bango.password,
+        ]).toString()
+        def res = IO.soapRequest(bango.url as String, req, [
+            Host: 'webservices.bango.com'
+        ])
+        return [
+            /*
+                OK              Success
+                ACCESS_DENIED   Invalid username/password
+                ACCESS_DENIED   Invalid client IP
+                ACCESS_DENIED           Invalid access to specified subscription ID
+                INVALID_SUBSCRIPTIONID Invalid subscription ID
+                INTERNAL_ERROR  A problem on the server meant that the request could not be processed
+                RETRYING        Renewal for this subscription period has been attempted, but failed. A retry will soon be attempted at the date now set as the next renewal date.
+                UNCOLLECTED     All retry attempts for the latest renewal have failed, so this period has not been paid for.
+                REVOKED         Subscription has been cancelled and any remaining paid- for time has been revoked.
+                INACTIVE        When a subscription paid for using credit card has not been accessed by a user for at least 2 subscription periods
+                WAITING         A payment collection attempt has been made, but the biller has not yet confirmed whether this was successful.
+                CANCELLED       Subscription has been cancelled by you, the user, or possibly because the operator let Bango know the user’s phone number has been disconnected or recycled
+                ERROR           Payment has failed.
+            */
+            status: res.Body.GetSubscriptionInfoResponse.GetSubscriptionInfoResult.responseCode as String
+        ]
+    }
+
+    def cancel(BillingEvent e) {
+        def req = bango.tmpl.cancel.make([
+            event: e
+        ]).toString()
+        def res = IO.soapRequest(bango.url as String, req, [
+            Host: 'webservices.bango.com'
+        ])
+        return [
+            /*
+                OK              Success
+                ACCESS_DENIED   Invalid username/password
+                ACCESS_DENIED   Invalid client IP address
+                ACCESS_DENIED   Invalid access to specified subscription ID
+                INVALID_SUBSCRIPTIONID  Invalid subscription ID
+                ALREADY_CANCELLED   Subscription already cancelled
+                INTERNAL_ERROR A problem on the server meant that the request could not be processed
+            */
+            status: res.Body.CancelSubscriptionResponse.CancelSubscriptionResult.responseCode as String
+        ]
+    }
+
 }
